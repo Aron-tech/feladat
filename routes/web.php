bce394420e26e2c2a636c61ef2434400c5c18549
Aron-tech <aron.papp2003@gmail.com>
2024-11-03 16:17:29 +0100

    Javítások2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Project;
 use App\Http\Controllers\ProjectController;
 
 Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/create', [ProjectController::class, 'edit'])->name('projects.create');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+
+Route::controller(ProjectController::class)->prefix('projects')->name('projects.')->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'edit')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{project}/edit', 'edit')->name('edit');
+    Route::put('/{project}', 'update')->name('update');
+});
